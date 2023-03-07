3c105472d81ff7602680dff8ffab07f4cba15afe
Natbag98 <100748789+Natbag98@users.noreply.github.com>
2023-03-07 23:00:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+TOKEN = os.environ['TOKEN']
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+async def load_extensions():
+    """
+    Get all the cogs from the cog folder and add them to the bot.
+    """
+    for file in os.listdir('./cogs'):
+        if file.endswith('.py'):
+            await bot.load_extension(f'cogs.{file[:-3]}')
+
+async def main():
+    """
+    Start and run the bot.\n
+    Must be run with asyncio.run()
+    """
+    async with bot:
+        await load_extensions()
+        await bot.start(TOKEN)
+
+asyncio.run(main())
